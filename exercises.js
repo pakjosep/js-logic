ddaa3c491d627eb67c132769fa57808d98664406
Joseph Pak <pakjosep@Josephs-MacBook-Air.local>
2017-08-12 20:34:11 -1000

    whattup

--- exercises.js
+++ exercises.js
@@ -165,8 +165,15 @@ console.log(myString);
  * If the strings are not equal, the function will return the message "Opposites do attract." Otherwise, return the message: "Cause it's like you're my mirror." 
  * Console.log your result.
 */
-
-
+function notEqual(first, second){
+	if (first!=second){
+		return "Opposites do attract.";
+	}else{
+		return "Cause it's like you're my mirror."
+	}
+}
+var myWords = notEqual("cat", "cat");
+console.log(myWords);
 /*
  * #8
  * Function - spareChange
