38499eaf5d6358db5e58035fb36f84db9ff35b17
Joseph Pak <pakjosep@Josephs-MacBook-Air.local>
2017-08-13 19:27:07 -1000

    Q13

--- exercises.js
+++ exercises.js
@@ -309,6 +309,18 @@ console.log(myProgress);
  * Console.log your result.
 */ 
 
+function moneyTrain(speed){
+	if (speed<50){
+		return "You are riding Honolulu's Rail.";
+	} else if (speed<100){
+		return "You are riding an Amtrak.";
+	} else if (speed>100){
+		return "Now you ballin' in the Shinkansen";
+	}
+	
+}
+var mySpeed = moneyTrain(200);
+console.log(mySpeed);
 
 /*
  * #14
